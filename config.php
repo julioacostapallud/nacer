6e7f4668bfb32207ac879295b346d78425b35882
julioacostapallud <julio.acosta@claro.com.ar>
2021-11-08 12:03:59 -0300

    Corrección config.php DB nacer

--- config.php
+++ config.php
@@ -54,7 +54,7 @@ $db_type = 'postgres8';				// Tipo de base de datos.
 $db_host = 'localhost';		// Host para desarrollo.
 $db_user = 'projekt';				// Usuario.
 $db_password = 'propcp';			// Contrase�a.
-$db_name = 'nacer2';
+$db_name = 'nacer';
 
 
 // IPs permitidas para conectarse al gestion, si el ip no esta aca, se envia un mail
